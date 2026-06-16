2bc5da2f13017d6da940712b60a03af8e5eb7e4d
pmonterde <paumonterdearias@gmail.com>
2026-06-16 12:04:12 +0200

    Fix: remover __pycache__ y aplicar .gitignore

--- engine/executor/executor.py
+++
@@ -1,5 +0,0 @@
-from engine.models.intent.models import Intent, INTENT_RULES
-from engine.models.executor.models import ExecutionResult
-
-def execute(intent:Intent):
-    return ExecutionResult(True)
